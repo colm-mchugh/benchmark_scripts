d20bb99018e458443b3ca94da777eca4e82e7352
Colm <colm.mchugh@gmail.com>
2025-04-03 17:58:35 +0100

    Create flamegraph.sh

---
+++ flamegraph.sh
@@ -0,0 +1,7 @@
+export FG=`pwd`FlameGraph
+perf record -F 999 -p 1 --call-graph dwarf sleep $2
+echo "Finished perf record"
+perf script | $FG/stackcollapse-perf.pl > out.perf-folded
+echo "Finished stackcollapse"
+cat out.perf-folded | $FG/flamegraph.pl > perf.$1.svg
+ls -ltrh perf.$1.svg
