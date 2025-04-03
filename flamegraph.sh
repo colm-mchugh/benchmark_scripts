4f4850d810374763f2cc166e4d401a34599959db
Colm <colm.mchugh@gmail.com>
2025-04-03 18:24:38 +0100

    Update flamegraph.sh

--- flamegraph.sh
+++ flamegraph.sh
@@ -3,5 +3,5 @@ perf record -F 999 -p 1 --call-graph dwarf sleep $2
 echo "Finished perf record"
 perf script | $FG/stackcollapse-perf.pl > out.perf-folded
 echo "Finished stackcollapse"
-cat out.perf-folded | $FG/flamegraph.pl > perf.$1.svg
+cat out.perf-folded | $FG/flamegraph.pl > cpu.$1.svg
 ls -ltrh perf.$1.svg
