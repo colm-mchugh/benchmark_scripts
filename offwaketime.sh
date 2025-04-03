81dc2d3d11bb1ba702b32517436459985777597f
Colm <colm.mchugh@gmail.com>
2025-04-03 18:27:13 +0100

    Update offwaketime.sh

--- offwaketime.sh
+++ offwaketime.sh
@@ -3,4 +3,4 @@ export FG=`pwd`FlameGraph
 $BCC/tools/offwaketime.py -f $2> offwaketime.out.stacks
 echo "Generated offwaketime stacks"
 $FG/flamegraph.pl --color=wakeup --title="Off Wakeup Time Flame Graph" --countname=us < offwaketime.out.stacks > offwaketime.$1.svg
-echo "Created offwaketime Flamegraph"
+ls -ltrh offwaketime.$1.svg
