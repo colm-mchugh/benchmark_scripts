88b482f9145b63d72602ee5ea009779e1bfcacfd
Colm <colm.mchugh@gmail.com>
2025-04-03 18:26:53 +0100

    Update offcputime.sh

--- offcputime.sh
+++ offcputime.sh
@@ -3,4 +3,4 @@ export FG=`pwd`FlameGraph
 $BCC/tools/offcputime.py -df -p 1 $2 > offcputime.out.stacks
 echo "Finished offcputime tracing"
 $FG/flamegraph.pl --color=io --title="Off-CPU Time Flame Graph" --countname=us < offcputime.out.stacks > offcputime.$1.svg
-echo "Finished FlameGraph generation"
+ls -ltrh offcputime.$1.svg
