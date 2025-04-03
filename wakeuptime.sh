c946196e562ef954c23837d9e4db5900988b72aa
Colm <colm.mchugh@gmail.com>
2025-04-03 18:19:32 +0100

    Update wakeuptime.sh

--- wakeuptime.sh
+++ wakeuptime.sh
@@ -1,6 +1,6 @@
 export BCC=`pwd`bcc
 export FG=`pwd`FlameGraph
-$BCC/bcc/tools/wakeuptime.py -f $2 > wakeuptime.out.stacks
+$BCC/tools/wakeuptime.py -f $2 > wakeuptime.out.stacks
 echo "Generated wakeup stacks"
 $FG/flamegraph.pl --color=wakeup --title="Wakeup Time Flame Graph" --countname=us < wakeuptime.out.stacks > wakeuptime.$1.svg
 echo "Created wakeup Flame Graph"
