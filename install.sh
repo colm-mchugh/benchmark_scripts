3a2093397897016554e2d8b6a83b6d36b15458bb
Colm <colm.mchugh@gmail.com>
2025-04-07 09:54:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
+# Obtain FlameGraph and bcc sources
+git clone https://github.com/brendangregg/FlameGraph
+git clone https://github.com/iovisor/bcc
+
+export BCC=`pwd`bcc
+export FG=`pwd`FlameGraph
+
 export PATH=$PATH:`pwd`
 echo $PATH
