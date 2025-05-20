a972147d3301cb4a5f9e106676a6ede4a63ce87a
eaydingol <ebruaydin@gmail.com>
2025-05-20 08:14:39 +0000

    add parameters to generator script

--- pgbench_scripts/gen_pgbench_script.py
+++ pgbench_scripts/gen_pgbench_script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import itertools
 
 clients = ['128', '256', '385', '512']
 instances = [ { "PGHOST": "ec1.postgres.database.azure.com", 
@@ -24,6 +25,22 @@ instances = [ { "PGHOST": "ec1.postgres.database.azure.com",
                "PGPASSWORD": "Oranje11", 
                "PGDATABASE": "postgres"  },] 
 
+# Parameters to set in the server. 
+server_parameters = [ 
+  {"name": "citus.max_adaptive_executor_pool_size", "values": [1, 2]}, 
+#  {"name": "citus.max_cached_conns_per_worker", "values": [1, 2, 4, 8, 16, 32]},
+  {"name": "pgbouncer", "values": ["on", "off"]}
+]
+
+
+param_names = [param["name"] for param in server_parameters]
+param_values = [param["values"] for param in server_parameters]
+
+parameter_combinations = []
+for values in itertools.product(*param_values):
+  combination = {name: value for name, value in zip(param_names, values)}
+  parameter_combinations.append(combination)
+
 fw = open('run_pgbench.sh', 'w')
 fw.write('#!/bin/bash\n')
 fw.write('clients=(' + ' '.join(clients) + ')\n')
@@ -35,6 +52,9 @@ fw.write('  for c in "${clients[@]}"\n')
 fw.write('  do\n')
 
 for instance in instances:
+  for param_combination in parameter_combinations:
+    for param_name, param_value in param_combination.items():
+      fw.write(f'    az postgres flexible-server parameter set --resource-group XXX --server-name {instance["PGHOST"]} --name {param_name} --value {param_value}\n')
     for env_var, value in instance.items():
         fw.write(f"    export {env_var}={value}\n")
     fw.write('    echo "PGHOST:" `env|grep PGHOST` \n')
